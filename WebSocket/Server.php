512df0c75e75f25589c059eb981ae76fd5a83021
zhaocong6 <1140253608@qq.com>
2018-07-23 09:49:45 +0800

    增加ws服务监听关闭功能

--- WebSocket/Server.php
+++ WebSocket/Server.php
@@ -10,7 +10,12 @@ $server->on('message', function (swoole_websocket_server $server, $frame) {
     echo 'ws客户端数据:'.$frame->data;
 
     $server->tick(1000, function ($id)use ($server, $frame){
-        !$server->push($frame->fd, '你好客户端') && $server->clearTimer($id);
+        if ($server->exist($frame->fd)){
+            $server->push($frame->fd, '你好客户端');
+        }else{
+            $server->clearTimer($id);
+            $server->close($frame->fd);
+        }
     });
 });
 
